90a4da83fc839c764c2aa0f851890038585776eb
luis lorenzo <sixsixonelui@gmail.com>
2019-01-21 14:18:56 -0400

    Cambios !!!

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,7 +7,7 @@ namespace DAL
     {
         public DbSet<Usuario> Usuario { get; set; }
         public DbSet<Producto> Producto { get; set; }
-        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Cliente7> Clientes { get; set; }
         public DbSet<Factura> factura { get; set; }
         public DbSet<FacturaDetalle> Facturas { get; set; }
 
